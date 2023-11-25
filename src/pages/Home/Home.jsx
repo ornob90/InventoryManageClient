062e168d58c82c5d2fab69d927c009d4520599dd
ornob90 <oornob49@gmail.com>
2023-11-25 09:01:48 +0600

    business states complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import BusinessStates from "./BusinessStates";
 
 const Home = () => {
   return (
     <div className="">
       <Banner />
+      <BusinessStates />
     </div>
   );
 };
