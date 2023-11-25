fd4334da871f1161f512cdb4fb1bc854b7b0ba7f
ornob90 <oornob49@gmail.com>
2023-11-25 11:47:07 +0600

    dashboard nav and drawer respnosive complete

--- src/pages/Dashboard/Navigation/DashboardNav.jsx
+++ src/pages/Dashboard/Navigation/DashboardNav.jsx
@@ -31,7 +31,7 @@ const DashboardNav = () => {
   ];
 
   return (
-    <div>
+    <div className="relative">
       <div className="flex items-center justify-between py-2 pl-4 text-2xl font-bold text-white bg-primary">
         <div className="flex items-center gap-2">
           {menuOpen ? (
@@ -46,12 +46,19 @@ const DashboardNav = () => {
         </div>
       </div>
       {/* Drawer */}
-      <div className="w-full bg-gray-400">
-        <div className=" h-[calc(100vh-48px)]  bg-gray-700 w-[20%] min-h-[500px]">
+      <div className={`${menuOpen ? "w-full h-screen bg-gray-400" : "w-auto"}`}>
+        <div
+          className={` h-[calc(100vh-48px)]  bg-gray-700 w-[60%] sm:w-[40%] md:w-[30%] lg:w-[20%] min-h-[500px] fixed top-[48px]  ${
+            menuOpen ? "left-0" : "left-[-100%]"
+          } duration-[.3s]`}
+        >
           <ul className="flex flex-col gap-4 pt-4 font-semibold text-white text-medium">
             {navItems.map(({ name, slug }) => (
               <NavLink
-                onClick={() => setActiveNav(slug)}
+                onClick={() => {
+                  setActiveNav(slug);
+                  setMenuOpen(false);
+                }}
                 to={slug}
                 key={name}
                 className={
