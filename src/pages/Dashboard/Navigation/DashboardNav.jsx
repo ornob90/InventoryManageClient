34fab91d53bde2d6f97c533d45827411dc073308
ornob90 <oornob49@gmail.com>
2023-11-25 11:41:19 +0600

    dashboard toggle menu icon close and open complete

--- src/pages/Dashboard/Navigation/DashboardNav.jsx
+++ src/pages/Dashboard/Navigation/DashboardNav.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { MdOutlineMenu } from "react-icons/md";
+import { IoCloseOutline } from "react-icons/io5";
 
 const DashboardNav = () => {
   const [activeNav, setActiveNav] = useState("/dashboard");
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const navItems = [
     {
@@ -30,26 +33,36 @@ const DashboardNav = () => {
   return (
     <div>
       <div className="flex items-center justify-between py-2 pl-4 text-2xl font-bold text-white bg-primary">
-        <h1>
-          <Link to="/">InventorHub</Link>
-        </h1>
+        <div className="flex items-center gap-2">
+          {menuOpen ? (
+            <IoCloseOutline onClick={() => setMenuOpen(false)} className="" />
+          ) : (
+            <MdOutlineMenu onClick={() => setMenuOpen(true)} className="" />
+          )}
+
+          <h1>
+            <Link to="/">InventorHub</Link>
+          </h1>
+        </div>
       </div>
       {/* Drawer */}
-      <div className=" h-[calc(100vh-48px)]  bg-gray-700 w-[20%]">
-        <ul className="flex flex-col gap-4 pt-4 font-semibold text-white text-medium">
-          {navItems.map(({ name, slug }) => (
-            <NavLink
-              onClick={() => setActiveNav(slug)}
-              to={slug}
-              key={name}
-              className={
-                activeNav === slug ? "bg-primary pl-4 py-2" : "pl-4 py-2"
-              }
-            >
-              {name}
-            </NavLink>
-          ))}
-        </ul>
+      <div className="w-full bg-gray-400">
+        <div className=" h-[calc(100vh-48px)]  bg-gray-700 w-[20%] min-h-[500px]">
+          <ul className="flex flex-col gap-4 pt-4 font-semibold text-white text-medium">
+            {navItems.map(({ name, slug }) => (
+              <NavLink
+                onClick={() => setActiveNav(slug)}
+                to={slug}
+                key={name}
+                className={
+                  activeNav === slug ? "bg-primary pl-4 py-2" : "pl-4 py-2"
+                }
+              >
+                {name}
+              </NavLink>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
