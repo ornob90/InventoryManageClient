bbebab45e4678320843d941124f6b5b28cc79270
ornob90 <oornob49@gmail.com>
2023-11-26 14:47:47 +0600

    add to cart functionality complete

--- src/pages/Dashboard/Manager/SalesCollection/SalesProductTable.jsx
+++ src/pages/Dashboard/Manager/SalesCollection/SalesProductTable.jsx
@@ -4,8 +4,41 @@ import { MdShoppingCartCheckout } from "react-icons/md";
 import { Link } from "react-router-dom";
 import useAuth from "../../../../hooks/auth/useAuth";
 import useGetSecure from "../../../../hooks/apiSecure/useGetSecure";
+import Swal from "sweetalert2";
+import usePostSecure from "../../../../hooks/apiSecure/usePostSecure";
+import toast from "react-hot-toast";
 
 const SalesProductTable = ({ products }) => {
+  const { mutateAsync: addToCart } = usePostSecure(null, "/cart");
+
+  const handleCart = (id) => {
+    const cartProduct = {
+      product: id,
+    };
+
+    Swal.fire({
+      title: "Add to cart?",
+      text: "You can always delete this from checkout page",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Add",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        addToCart(cartProduct)
+          .then((res) => {
+            if (res.insertOne) {
+              toast.success("Product added to cart!");
+            }
+          })
+          .catch((err) =>
+            toast.error(`Unable to add the product to cart  ${err.message}`)
+          );
+      }
+    });
+  };
+
   return (
     <div className="mt-5 overflow-x-auto min-h-[300px]">
       <table className="table text-[12px] ">
@@ -40,7 +73,10 @@ const SalesProductTable = ({ products }) => {
               <td>{product?.discount}%</td>
               <td>{product?.saleCount}</td>
               <td className="flex items-center h-full gap-2 pt-5 text-2xl">
-                <MdShoppingCartCheckout className="text-primary" />
+                <MdShoppingCartCheckout
+                  onClick={() => handleCart(product?._id)}
+                  className="text-primary text-3xl"
+                />
               </td>
             </tr>
           ))}
