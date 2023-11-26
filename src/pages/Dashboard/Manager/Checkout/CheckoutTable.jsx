25f81c10b140811d3346c31fc61f8ab0ad7e9051
ornob90 <oornob49@gmail.com>
2023-11-26 15:14:36 +0600

    delete from curt functionality complete

--- src/pages/Dashboard/Manager/Checkout/CheckoutTable.jsx
+++ src/pages/Dashboard/Manager/Checkout/CheckoutTable.jsx
@@ -2,11 +2,33 @@ import React from "react";
 import { AiFillEdit } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import useDeleteSecure from "../../../../hooks/apiSecure/useDeleteSecure";
 
 const CheckoutTable = ({ cartProducts }) => {
   console.log(cartProducts);
+
+  const { mutateAsync: deleteProduct } = useDeleteSecure([["Cart"]]);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteProduct(`/cart/${id}`).then((res) => console.log(res));
+        // console.log(id, product?._id, product);
+      }
+    });
+  };
+
   return (
-    <div className="mt-10 overflow-x-auto">
+    <div className="mt-10 overflow-x-auto  min-h-[400px]">
       <table className="table text-[12px] md:text-sm">
         {/* head */}
         <thead className="font-bold text-black">
@@ -37,7 +59,10 @@ const CheckoutTable = ({ cartProducts }) => {
               <td>{product?.product?.productLocation}</td>
               <td>{product?.product?.sellingPrice}</td>
               <td className="flex items-center h-full gap-2 pt-5 text-3xl">
-                <MdDelete className="text-red-600" />
+                <MdDelete
+                  onClick={() => handleDelete(product?._id)}
+                  className="text-red-600"
+                />
               </td>
             </tr>
           ))}
