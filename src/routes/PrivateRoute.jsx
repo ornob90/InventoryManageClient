896ecdd5726834be3a53eb4bc73e7f2b915829fc
ornob90 <oornob49@gmail.com>
2023-11-26 21:26:44 +0600

    private route component complete

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import useAuth from "../hooks/auth/useAuth";
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../pages/Loading/Loading";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const { pathname } = useLocation();
+
+  if (loading) {
+    return <Loading />;
+  } else if (user) {
+    return children;
+  }
+  return <Navigate state={pathname} to="/login"></Navigate>;
+};
+
+export default PrivateRoute;
