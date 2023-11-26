86bd02649c395d8688a86c6e3c2c7ce7a3bf6cba
ornob90 <oornob49@gmail.com>
2023-11-26 09:30:30 +0600

    sign out complete in nav

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import AuthProvider from "./providers/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <RouterProvider router={router} />
+        <Toaster />
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
--- src/pages/Home/Navbar.jsx
+++ src/pages/Home/Navbar.jsx
@@ -261,7 +261,7 @@ const Navbar = () => {
             </div>
 
             <Button
-              //   onClick={handleSignOut}
+              onClick={handleSignOut}
               className="px-2 py-1 text-sm text-white rounded-sm bg-primary md:py-2 md:px-5 md:text-base dark:bg-dark-text dark:text-dark-mode"
             >
               Sign Out
